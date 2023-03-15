3bfbe1c92385ae6121d9b5552f9984b618c67740
João Pedro <11joao44@gmail.com>
2023-03-15 16:52:14 -0400

    criando verificação dos operadores

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
-<script setup>
+<script>
+import Calculadora from './components/Calculadora.vue';
 
+export default {
+  name: 'App',
+  components: {
+    Calculadora,
+  },
+};
 </script>
 
 <template>
-
+  <div class="container">
+    <header class="p-3 mb-4 mt-4 bg-dark text-center text-white rounded-3">
+      <h1>Calculadora Aritmética</h1>
+    </header>
+    <div id="app">
+      <Calculadora />
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    data() {
+        return {
+            numero1: '',
+            numero2: '',
+            operacao: '',
+        };
+    },
+    computed: {
+        resultado() {
+            if (this.operacao === 'soma') {
+                return Number(this.numero1) + Number(this.numero2);
+            } else if (this.operacao === 'subtracao') {
+                return Number(this.numero1) - Number(this.numero2);
+            } else if (this.operacao === 'multiplicacao') {
+                return Number(this.numero1) * Number(this.numero2);
+            } else if (this.operacao === 'divisao') {
+                return Number(this.numero1) / Number(this.numero2);
+            }
+        },
+    },
+};
+</script>
+
+
+<template>
+    <div>
+        <div class="d-flex justify-content-evenly">
+            <input type="number" v-model="numero1" />
+            <select v-model="operacao">
+                <option value="soma">+</option>
+                <option value="subtracao">-</option>
+                <option value="multiplicacao">x</option>
+                <option value="divisao">÷</option>
+            </select>
+            <input type="number" v-model="numero2" />
+        </div>
+        <div class="text-center mt-5">Resultado: {{ resultado }}</div>
+    </div>
+</template>
